d8728f03102b99f5a151859ac5e6b3f267d2d4b4
Robin Salen <salenrobin@gmail.com>
2022-10-13 19:27:53 -0400

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! of the Anemoi permutation, and applications to a Sponge mode and a
 //! novel Jive compression mode.
 //!
-//! All hash instantiations are defined using an `Sponge` trait and can both
+//! All hash instantiations are defined using a `Sponge` trait and can both
 //! process sequences of bytes or native field elements.
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
@@ -19,37 +19,44 @@ extern crate alloc;
 mod traits;
 pub use traits::*;
 
-/// An implementation of some instantiations of the Anemoi permutation
-/// in Sponge and Jive modes over BLS12-377 base field.
+/// An implementation of instantiations of the Anemoi permutation
+/// in Sponge and Jive modes targetting the 128-bit security level
+/// over BLS12-377 base field.
 #[cfg(feature = "bls_377")]
 pub mod bls12_377;
 
-/// An implementation of some instantiations of the Anemoi permutation
-/// in Sponge and Jive modes over BLS12-381 base field.
+/// An implementation of instantiations of the Anemoi permutation
+/// in Sponge and Jive modes targetting the 128-bit security level
+/// over BLS12-381 base field.
 #[cfg(feature = "bls_381")]
 pub mod bls12_381;
 
-/// An implementation of some instantiations of the Anemoi permutation
-/// in Sponge and Jive modes over BN-254 base field.
+/// An implementation of instantiations of the Anemoi permutation
+/// in Sponge and Jive modes targetting the 128-bit security level
+/// over BN-254 base field.
 #[cfg(feature = "bn_254")]
 pub mod bn_254;
 
-/// An implementation of some instantiations of the Anemoi permutation
-/// in Sponge and Jive modes over ED_ON_BLS12-377 base field.
+/// An implementation of instantiations of the Anemoi permutation
+/// in Sponge and Jive modes targetting the 128-bit security level
+/// over ED_ON_BLS12-377 base field.
 #[cfg(feature = "ed_on_bls12_377")]
 pub mod ed_on_bls12_377;
 
-/// An implementation of some instantiations of the Anemoi permutation
-/// in Sponge and Jive modes over Jubjub base field.
+/// An implementation of instantiations of the Anemoi permutation
+/// in Sponge and Jive modes targetting the 128-bit security level
+/// over Jubjub base field.
 #[cfg(feature = "jubjub")]
 pub mod jubjub;
 
-/// An implementation of some instantiations of the Anemoi permutation
-/// in Sponge and Jive modes over Pallas base field.
+/// An implementation of instantiations of the Anemoi permutation
+/// in Sponge and Jive modes targetting the 128-bit security level
+/// over Pallas base field.
 #[cfg(feature = "pallas")]
 pub mod pallas;
 
-/// An implementation of some instantiations of the Anemoi permutation
-/// in Sponge and Jive modes over Vesta base field.
+/// An implementation of instantiations of the Anemoi permutation
+/// in Sponge and Jive modes targetting the 128-bit security level
+/// over Vesta base field.
 #[cfg(feature = "vesta")]
 pub mod vesta;
